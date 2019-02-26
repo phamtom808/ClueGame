d3a914155ac99c29e2feafe719d2dcd5003ef312
Dane Pham <dpham@mines.edu>
2019-02-26 11:59:08 -0700

    Finished Tests for corners and edges

---
+++ src/tests/IntBoardTests.java
@@ -0,0 +1,71 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import experiments.BoardCell;
+
+class IntBoardTests {
+
+	@Before
+	public void beforeAll() {
+		board = new IntBoard(); // constructor should call calcAdjacencies()
+	}
+	
+	/**
+	 * Test adjacencies for top left corner
+	 */
+	@Test
+	public void testAdjacency0() {
+		BoardCell cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1,0)));
+		assertTrue(testList.contains(board.getCell(0,1)));
+		assertEquals(2, testList.size());
+	}
+	
+	/**
+	 * Test adjacencies for bottom left corner
+	 */
+	@Test
+	public void testAdjacency3() {
+		BoardCell cell = board.getCell(3,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(2,3)));
+		assertTrue(testList.contains(board.getCell(3,2)));
+		assertEquals(2, testList.size());
+	}
+	
+	/**
+	 * Test adjacencies for a right edge
+	 */
+	@Test
+	public void testAdjacencyRightEdge() {
+		BoardCell cell= board.getCell(1,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(0,3)));
+		assertTrue(testList.contains(board.getCell(2,3)));
+		assertTrue(testList.contains(board.getCell(1,2)));
+		assertEquals(3,testList.size());
+	}
+	
+	/**
+	 * Test adjacencies for a left edge
+	 */
+	public void testAdjacencyLeftEdge() {
+		BoardCell cell = board.getCell(3,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(2,0)));
+		assertTrue(testList.contains(board.getCell(3,1)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
