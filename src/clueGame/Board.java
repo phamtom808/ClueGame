82193ee49dac838f918f657549c1c0fb68c3067f
Emily Christensen <emilychristensen@mines.edu>
2019-02-28 11:53:08 -0700

    Adds more methods to Board and BoardCell class, refactors calcAdjacencies so that the actual calculation will be done by BoardCell, not board

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,27 @@
 package clueGame;
 
 public class Board {
-
+	
+	public static final int MAX_BOARD_SIZE = 50;
+	public int numRows;
+	public int numColumnss;
+	
+	
+	private static Board theInstance;
+	
+	private Board() {}
+	
+	public static Board getInstance() {
+		return theInstance;
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	
 }
--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -1,5 +1,5 @@
 package clueGame;
 
 public enum DoorDirection {
-
+	
 }
