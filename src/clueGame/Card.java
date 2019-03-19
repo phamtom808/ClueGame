e23e529d8e5d615202b0f082b96ab65f5d32d7fd
Emily Christensen <emilychristensen@mines.edu>
2019-03-19 11:24:07 -0600

    Adds files for card, players, cardType enum

---
+++ src/clueGame/Card.java
@@ -0,0 +1,26 @@
+package clueGame;
+
+public class Card {
+	private CardType cardType;
+	private String name;
+	public Card(String n, CardType ctype) {
+		this.name = n;
+		this.cardType = ctype;
+	}
+	
+	public boolean equals(Card other) {
+		if(this.name == other.getName() && this.cardType == other.getCardType())) {
+			return true;
+		}
+		return false;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public CardType getCardType() {
+		return this.cardType;
+	}
+	
+}
---
+++ src/clueGame/Player.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class Player {
+	public String name;
+	private int row;
+	private int column;
+	private Color color;
+	boolean isHumanPlayer;
+	set<Card> hand;
+}
