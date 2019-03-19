d90b01d741cdd91e918d0f12ca2bd5be3c7d733f
Dane Pham <dpham@mines.edu>
2019-03-19 11:44:08 -0600

    finished setUp class for tests

--- src/tests/MyGameSetupTests.java
+++ src/tests/MyGameSetupTests.java
@@ -9,13 +9,20 @@ import clueGame.Board;
 public class MyGameSetupTests {
 	
 	private static Board board;
-	@Before
+	
+	public static final int NUM_PLAYERS = 6;
+	public static final int NUM_WEAPONS = 6;
+	
+	@BeforeClass
 	public static void setUp() {
 		board = Board.getInstance();
 		board.setConfigFiles("BoardLayout.csv", "ClueRooms.txt");
 		board.setNewConfig("CluePlayers.txt", "ClueWeapons.txt");
 		board.initialize();
 	}
+	
+	
+	
 
 
 }
