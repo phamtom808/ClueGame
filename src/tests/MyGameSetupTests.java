90239204fb5e981c83c91d605cebc9f0c3bc12a4
Dane Pham <dpham@mines.edu>
2019-03-19 12:15:04 -0600

    started test of players

--- src/tests/MyGameSetupTests.java
+++ src/tests/MyGameSetupTests.java
@@ -2,9 +2,14 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.util.Set;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import clueGame.Board;
+import clueGame.Player;
 
 public class MyGameSetupTests {
 	
@@ -21,7 +26,27 @@ public class MyGameSetupTests {
 		board.initialize();
 	}
 	
-	
+	@Test
+	public void testPlayers() {
+		//arrayList or Set players
+		Set<Player> players = board.getPlayers();
+		Player arrayOfPlayers[] = new Player[6];
+		arrayOfPlayers = players.toArray(arrayOfPlayers);
+		//tests if the correct number of players are loaded
+		assertEquals(NUM_PLAYERS, players.size());
+		//test if set contains a human player
+		assertTrue(players.contains(Miss Scarlett));
+		//test if set contains all computer players
+		assertTrue(players.contains(Colonel Mustard));
+		assertTrue(players.contains(Mrs. White));
+		assertTrue(players.contains(Mr. Green));
+		assertTrue(players.contains(Professor Plum));
+		assertTrue(players.contains(Miss Peach));
+		//test if first player is correct
+		assertEquals("Miss Scarlet", arrayOfPlayers[0].name);
+		assertEquals(Color.RED, arrayOfPlayers[0].getColor());
+		assertTrue(arrayOfPlayers[0].getHumanPlayer());
+	}
 	
 
 
