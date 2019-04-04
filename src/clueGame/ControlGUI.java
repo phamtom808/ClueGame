e6f2e5db00c0b3222af4e9a0d8303fa686ed79af
Emily Christensen <emilychristensen@mines.edu>
2019-04-04 11:57:51 -0600

    Adds in Guess wrapper class to handle the guesses, since the code was getting ugly

--- src/clueGame/ControlGUI.java
+++ src/clueGame/ControlGUI.java
@@ -2,6 +2,8 @@ package clueGame;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,16 +15,42 @@ import javax.swing.border.TitledBorder;
 public class ControlGUI extends JPanel{
 	
 	public ControlGUI() {
-		
+		setLayout(new GridLayout(3,1)); //3 rows, each with one panel element
+		add(this.createTextDisplays());
+		add(this.createButtons()); // bottom row contains the buttons
+	}
+	
+	private JPanel createTextDisplays() {
+		JPanel textDisplay = new JPanel();
+		textDisplay.setLayout(new GridLayout(1,2)); //one row, 2 items: die roll and guess handler
+		JPanel guessHandler = new JPanel(); 
+		guessHandler.setLayout(new GridLayout(2,1)); //2 rows, 1 column: top is the guess, bottom is the result
+		JTextField lastGuess = new JTextField();
+		JTextField lastGuessResult = new JTextField();
+		JTextField dieRoll = new JTextField();
+	}
+	
+	private JPanel createButtons() {
+		JPanel buttonMenu = new JPanel();
+		buttonMenu.setLayout(new GridLayout(1,2));
+		JButton makeAccusation = new JButton("Make Accusation");
+		JButton nextPlayer = new JButton("Next Player");
+		buttonMenu.add(makeAccusation);
+		buttonMenu.add(nextPlayer);
+		return buttonMenu;
 	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Clue");
+		frame.setSize(500,200);
 		ControlGUI gui = new ControlGUI();
 		frame.add(gui, BorderLayout.CENTER);
 		frame.setVisible(true);
 	}
 	
+	public String getStringGuess() {
+		return "It was: "; 
+	}
 }
