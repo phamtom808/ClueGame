718e7713608a1befe853a272d31c6616d510dbab
Dane Pham <dpham@mines.edu>
2019-04-04 00:15:10 -0600

    finished correct accusation tests. finished calculating target test. working on create/disprove/handle suggestion tests.

--- src/tests/MyGameActionTest.java
+++ src/tests/MyGameActionTest.java
@@ -10,7 +10,9 @@ import java.util.Set;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import clueGame.BadConfigFormatException;
 import clueGame.Board;
+import clueGame.BoardCell;
 import clueGame.Card;
 import clueGame.CardType;
 import clueGame.ComputerPlayer;
@@ -30,44 +32,150 @@ public class MyGameActionTest {
 	}
 	
 	@Test
-	public void selectTargetLocationComp() {
-		ArrayList<Player> players = board.getPlayers();
-		Player computerPlayer = new ComputerPlayer("Bob", )
-
+	public void selectTargetLocationComp() throws BadConfigFormatException {
+		//random selection test
+		try {
+			ComputerPlayer compPlayer = new ComputerPlayer("Bob", "Blue",17,6,board);
+			board.calcTargets(16,5,1);
+			boolean loc_16_6 = false;
+			boolean loc_17_5 = false;
+			boolean loc_15_5 = false;
+			
+			for(int i = 0; i < 100; i++) {
+				BoardCell selected = compPlayer.selectTarget(board);
+				if(selected == board.getCellAt(16,6)) {
+					loc_16_6 = true;
+				}
+				else if(selected == board.getCellAt(17,5)) {
+					loc_17_5 = true;
+				}
+				else if(selected == board.getCellAt(15,5)) {
+					loc_15_5 = true;
+				}
+				else {
+					fail("Invalid target.");
+				}
+			}
+			assertTrue(loc_17_7);
+			assertTrue(loc_16_6);
+			assertTrue(loc_18_6);
+		}catch (BadConfigFormatException e) {
+			throw new BadConfigFormatException("computer player did not intiailize correctly");
 		}
-		
+		//must select room if wasn't previously visited
+		try {
+			ComputerPlayer compPlayer = new ComputerPlayer("Rick", "Red",0,14,board);
+			board.calcTargets(0,14,1);
+			BoardCell selected = compPlayer.selectTarget(board);
+			assertTrue(selected.isRoom());
+		}catch (BadConfigFormatException e) {
+			throw new BadConfigFormatException("computer player did not intiailize correctly");
+		}
+		//if room just visited each target selected randomly
 	}
 	
 	/*
 	 * need to make an accusation class and a getter for accusation in board class
 	 */
 	@Test
-	public void checkAccusation() {
+	public void checkCorrectAccusation() {
+		ArrayList<Card> testCards = new ArrayList<Card>();
+		Card testWeapon = new Card("Rope", CardType.WEAPON);
+		Card testPlayer = new Card("Colonel Mustard", CardType.PLAYER);
+		Card testRoom = new Card("Pool", CardType.ROOM);
+		testCards.add(testWeapon);
+		testCards.add(testPlayer);
+		testCards.add(testRoom);
+		
+		assertTrue(testCards.contains(testWeapon));
+		assertEquals("Rope", testWeapon.getName());
+		assertEquals(CardType.WEAPON, testWeapon.getCardType());
+		assertTrue(testCards.contains(testPlayer));
+		assertEquals("Colonel Mustard", testPlayer.getName());
+		assertEquals(CardType.PLAYER, testPlayer.getCardType());
+		assertTrue(testCards.contains(testRoom));
+		assertEquals("Pool", testRoom.getName());
+		assertEquals(CardType.ROOM, testRoom.getCardType());
+	}
+	
+	@Test
+	public void checkWrongWeaponAccusation() {
+		ArrayList<Card> testCards = new ArrayList<Card>();
+		Card testWeapon = new Card("Rope", CardType.WEAPON);
+		Card testPlayer = new Card("Colonel Mustard", CardType.PLAYER);
+		Card testRoom = new Card("Pool", CardType.ROOM);
+		Card falseWeapon = new Card("Axe", CardType.WEAPON);
+		testCards.add(testWeapon);
+		testCards.add(testPlayer);
+		testCards.add(testRoom);
+		assertTrue(testCards.contains(testPlayer));
+		assertEquals("Colonel Mustard", testPlayer.getName());
+		assertEquals(CardType.PLAYER, testPlayer.getCardType());
+		assertTrue(testCards.contains(testRoom));
+		assertEquals("Pool", testRoom.getName());
+		assertEquals(CardType.ROOM, testRoom.getCardType());
+		assertEquals("Rope", falseWeapon.getName());
+	}
+	
+	@Test
+	public void checkWrongRoomAccusation() {
+		ArrayList<Card> testCards = new ArrayList<Card>();
 		Card testWeapon = new Card("Rope", CardType.WEAPON);
 		Card testPlayer = new Card("Colonel Mustard", CardType.PLAYER);
 		Card testRoom = new Card("Pool", CardType.ROOM);
 		Card falseRoom = new Card("Kitchen", CardType.ROOM);
-		Card falsePlayer = new Card("ABC", CardType.PLAYER);
-		Card falseWeapon = new Card("Axe", CardType.WEAPON);
-		
-		Accusation testAcc = new Accusation(testPlayer, testRoom, testWeapon);
-		assertEquals(board.getAccusation(), testAcc);
-		testAcc = new Accusation(testPlayer, falseRoom, testWeapon);
-		assertEquals(board.getAccusation(), testAcc);
-		testAcc = new Accusation(falsePlayer, testRoom, testWeapon);
-		assertEquals(board.getAccusation(), testAcc);
-		testAcc = new Accusation(testPlayer, testRoom, falseWeapon);
-		assertEquals(board.getAccusation(), testAcc);
-		
+		testCards.add(testWeapon);
+		testCards.add(testPlayer);
+		testCards.add(testRoom);
+		assertTrue(testCards.contains(testWeapon));
+		assertEquals("Rope", testWeapon.getName());
+		assertEquals(CardType.WEAPON, testWeapon.getCardType());
+		assertTrue(testCards.contains(testPlayer));
+		assertEquals("Colonel Mustard", testPlayer.getName());
+		assertEquals(CardType.PLAYER, testPlayer.getCardType());
+		assertTrue(testCards.contains(testRoom));
+		assertEquals("Pool", falseRoom.getName());
 	}
 	
 	@Test
-	public void createSuggestion() {
-		
+	public void checkWrongPlayerAccusation() {
+		ArrayList<Card> testCards = new ArrayList<Card>();
+		Card testWeapon = new Card("Rope", CardType.WEAPON);
+		Card testPlayer = new Card("Colonel Mustard", CardType.PLAYER);
+		Card testRoom = new Card("Pool", CardType.ROOM);
+		Card falsePlayer = new Card("ABC", CardType.PLAYER);
+		testCards.add(testWeapon);
+		testCards.add(testPlayer);
+		testCards.add(testRoom);
+		assertTrue(testCards.contains(testWeapon));
+		assertEquals("Rope", testWeapon.getName());
+		assertEquals(CardType.WEAPON, testWeapon.getCardType());
+		assertTrue(testCards.contains(testPlayer));
+		assertEquals("Colonel Mustard", testPlayer.getName());
+		assertEquals(CardType.PLAYER, testPlayer.getCardType());
+		assertTrue(testCards.contains(testRoom));
+		assertEquals("Colonel Mustard", falsePlayer.getName());
+	}
+	
+	@Test
+	public void createSuggestion() throws BadConfigFormatException {
+		try {
+			Card testRoom = new Card("Conservatory", CardType.ROOM);
+			ComputerPlayer compPlayer = new ComputerPlayer("Rick", "Red",0,5,board);
+			ArrayList<Card> suggestion = compPlayer.createSuggestion(board, testRoom);
+			
+		}catch (BadConfigFormatException e) {
+			throw new BadConfigFormatException("computer player did not intiailize correctly");
+		}
 	}
 	
 	@Test
 	public void disproveSuggestion() {
+		
+	}
+	
+	@Test
+	public void handleSuggestion() {
 		
 	}
 }
