fc3b629307fbe6c9c45cb6de8a918e3f10b4aed7
Dane Pham <dpham@mines.edu>
2019-04-18 11:38:03 -0600

    fixed controlGUI

--- src/clueGame/GuessGUI.java
+++ src/clueGame/GuessGUI.java
@@ -1,14 +1,20 @@
 package clueGame;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GuessGUI extends JPanel{
+	
+	private JPanel textDisplay;
+	private JPanel guessHandler;
+
 	
 	public GuessGUI() {
 		
 	}
 	
-	public void makeAGuess() {
-		
+	public void createRoom() {
+		JPanel panel = new JPanel();
+		panel.setLayout(gridLayout(1,2));
 	}
 }
